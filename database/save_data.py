391c0afd129a9554c7c81f400486dad0c7112922
jsebas96 <jbravomeneses@gmail.com>
2022-06-28 17:03:29 -0500

    Update subscribe topic

--- database/save_data.py
+++ database/save_data.py
@@ -16,7 +16,7 @@ from config.index import config
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe("electric-meter")
+    client.subscribe("<topic>")
 
 
 def on_message(client, userdata, msg):
