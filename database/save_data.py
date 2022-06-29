c31e7c5e5417f475f5d83f4991782842f4ff8f90
jsebas96 <jbravomeneses@gmail.com>
2022-06-29 16:53:05 -0500

    Database completed!!!

--- database/save_data.py
+++ database/save_data.py
@@ -11,6 +11,13 @@ from config.index import config
     CREATE MONGO CLIENT TO CONNECT DATABASE AND SAVE DATA
 '''
 
+# Connect to the MongoDB server
+db_client = MongoClient(config['MONGO_URI']) 
+# Create the database
+db_name = db_client['demo']
+# Create the collection 
+db_collection = db_name['electricMeter'] 
+
 
 #/////////////////////////////////////////////////////###
 
@@ -19,19 +26,26 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("rtu/#")
 
 
+# On Data Received Callback
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
-    '''
-        ON MESSAGE => ???
-    '''
+    # Convert from json to python
+    data = json.loads(msg.payload)
+    # Set date if not in data
+    if "date" in data:
+        data['date'] = datetime.fromtimestamp(data['date'])
+    else:
+        data['date'] = datetime.now()
+    # Save document in collection
+    db_collection.insert_one(data)
 
 
 # Generate a random client ID
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 # Create a new MQTT client instance
 client = mqtt_client.Client(client_id=client_id)
-client.username_pw_set(
-    username=config["USER"], password=config["PWD"])
+# client.username_pw_set(
+#     username=config["USER"], password=config["PWD"])
 client.connect(config['HOST'], int(config['PORT']),
                keepalive=60)  # Connect to the MQTT broker
 
